f1ed5f4dfe82be639b11eab79d12ff4f8cbf5839
Stephen Achugwo <dr_stephen@aol.com>
2025-05-25 03:41:36 +0100

    Update cloudinary.js

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,4 +1,5 @@
-import {v2 as cloudinary} from 'cloudinary'
+//import {v2 as cloudinary} from 'cloudinary'
+const cloudinary = require('cloudinary').v2;
 
 const connectCloudinary=async ()=> {
  cloudinary.config({
