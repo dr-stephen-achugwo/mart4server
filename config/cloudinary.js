6e78df5ba4fcda3d0743fdb4a26831cb698e5236
Stephen Achugwo <dr_stephen@aol.com>
2025-05-26 03:25:33 +0100

    Update cloudinary.js

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,16 +1,18 @@
-import dotenv from "dotenv";
+const cloudinary = require('cloudinary');
+const dotenv = require('dotenv');
+//import dotenv from "dotenv";
+//const { v2 } = cloudinary; 
+
 dotenv.config();
-import pkg from 'cloudinary';
-const {v2: cloudinary} = pkg;
 
 const connectCloudinary=async ()=> {
-  v2.config({  
+  cloudinary.config({  
     cloud_name: process.env.CLOUDINARY_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret:process.env.CLOUDINARY_SECRET_KEY
   })
 }
-export default connectCloudinary;
+module.exports = connectCloudinary;
 
 
 //module.exports = connectCloudinary;
